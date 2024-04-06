722d1289b4af5e040dd393af898720dc7c5d7577
Ujjwal Tiwari <ujtiwari@gmail.com>
2024-04-06 19:24:19 +0530

    deployment

--- Backend/server.js
+++ Backend/server.js
@@ -5,11 +5,12 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 import { app, server } from "./socket/socket.js";
 
 // const app = express();
 const PORT = process.env.PORT || 5000;
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
@@ -17,10 +18,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
